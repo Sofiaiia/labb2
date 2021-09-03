a273036cd9070f44c4890b9115fdc47936d2e9bd
sofia södergårdh <sofia.s@aland.net>
2021-09-03 16:11:47 +0300

    satt in lite kommentarer på vad jag gjort

--- quiz/src/views/Game.js
+++ quiz/src/views/Game.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 function Game(){
 
+    //Ska dessa va i reduxstoren kanske?
     const [questions,setQuestions] = useState([]);
     const [currentQuestion,setCurrent] =useState({});
 
+    //Hämta frågor från API med async/await 
     const fetchData = async() =>{
         try{
              const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
@@ -17,10 +19,12 @@ function Game(){
        
     }
 
+    //Hämta frågorna när vyn öppnas
     useEffect(()=>{
         fetchData();
     },[]);
 
+    //RENDERING
     return(
         <div>
           GAME
--- quiz/src/views/Start.js
+++ quiz/src/views/Start.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function Start(){
+
+    //Behövs denna???
     return(
         <div>
             Start 
