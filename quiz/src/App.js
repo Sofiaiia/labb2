267d79fa771195d21b5f74017df9535965e556d7
sofia södergårdh <sofia.s@aland.net>
2021-09-03 11:30:36 +0300

    har en meny

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,14 +1,26 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
 import './App.css';
+import Game from './views/Game';
+import Rules from './views/Rules';
+import Toplist from './views/Toplist';
+import Start from './views/Start';
 
 function App() {
   return (
     <div className="App">
+      <h1> QUIZ </h1>
       <BrowserRouter>
         <div>
-          <switch>
-            
-          </switch>
+          <Link to="/"> Home</Link>
+          <Link to="/game"> Play </Link>{' '}
+          <Link to="/rules"> Rules </Link>{' '}
+          <Link to="/toplist"> Toplist </Link>{' '}
+          <Switch>
+            <Route exact path="/" component={Start}/>
+            <Route path="/game" component={Game} /> 
+            <Route path="/rules" component={Rules} /> 
+            <Route path="/toplist" component={Toplist} /> 
+          </Switch>
         </div>
       </BrowserRouter>
     </div>
