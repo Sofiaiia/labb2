a19182700cc109336c6e23587f67b9677ce68f7f
sofia södergårdh <sofia.s@aland.net>
2021-09-13 08:53:53 +0300

    jobbar på reglerna

--- quiz/src/views/Rules.js
+++ quiz/src/views/Rules.js
@@ -3,7 +3,14 @@ import React from 'react';
 function Rules(){
     return(
         <div>
-            RULES
+            <h2> Rules: </h2>
+            <p> Choose the number of questions </p>
+            <p> Choose your difficulty </p>
+            <p> Start test </p>
+            <p> Read the question and the alternetives, choose the one you think is right </p>
+            <p> If question is right you get a point </p>
+            <p> When quiz is done, your poins are counted and your score and put in the toplist </p>
+            <h3> Good luck! </h3>
         </div>
     )
 }
