990bfa948d4985fd00a01698f13d461eb284851b
sofia södergårdh <sofia.s@aland.net>
2021-09-13 09:09:15 +0300

    lite styling också

--- quiz/src/views/Rules.js
+++ quiz/src/views/Rules.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../styling/rules.css';
 
 function Rules(){
     return(
         <div>
-            <h2> Rules: </h2>
+            <h2 className="headingRules"> Rules: </h2>
             <p> Choose the number of questions </p>
             <p> Choose your difficulty </p>
-            <p> Start test </p>
+            <p> Start test and the timer will start </p>
             <p> Read the question and the alternetives, choose the one you think is right </p>
-            <p> If question is right you get a point </p>
+            <p> If your answer is right you'll get a point </p>
             <p> When quiz is done, your poins are counted and your score and put in the toplist </p>
             <h3> Good luck! </h3>
         </div>
