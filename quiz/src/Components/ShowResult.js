035ef8745efb93c5cf0d79cb1c3baa0ab100e7e5
sofia södergårdh <sofia.s@aland.net>
2021-09-14 09:39:02 +0300

    fortätter med resultaten

--- quiz/src/Components/ShowResult.js
+++ quiz/src/Components/ShowResult.js
@@ -4,18 +4,22 @@ import { selectPoints,selectName,selectQuestions,selectTime} from '../store/resu
 
  function ShowResult(){
 
+    //sätt in nån bild från: https://pixabay.com/images/search/trophy/ 
+
     const totalPoints = useSelector(selectPoints);
     const name = useSelector(selectName);
     const numOfQuestions = useSelector(selectQuestions);
     const time = useSelector = useSelector(selectTime);
 
-    //plocka ut totala poängen 
-    //räkna up % rätt
     return(
         <div>
+            <h2> QUIZ COMPLETED </h2>
+
             <p> Total number of questions: {numOfQuestions} </p>
             <p> Totalt points: {totalPoints} </p>
+            <p> That means you have XX% right </p>
             <p> Your time: {time}</p>
+            <h3> <strong> GOOD JOB {name}</strong> - you can now find your result in the toplist! </h3>
         </div>
     )
  }
