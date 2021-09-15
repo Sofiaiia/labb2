14353eb0724df96dabb136bb1a9d5fc1ef2ee92d
sofia södergårdh <sofia.s@aland.net>
2021-09-15 16:00:22 +0300

    ett försök att göra rules lite roligare

--- quiz/src/views/Rules.js
+++ quiz/src/views/Rules.js
@@ -3,14 +3,14 @@ import '../styling/rules.css';
 
 function Rules(){
     return(
-        <div>
+        <div className="box">
             <h2 className="headingRules"> Rules: </h2>
-            <p> Choose the number of questions </p>
-            <p> Choose your difficulty </p>
-            <p> Start test and the timer will start </p>
-            <p> Read the question and the alternetives, choose the one you think is right </p>
-            <p> If your answer is right you'll get a point </p>
-            <p> When quiz is done, your poins are counted and your score and put in the toplist </p>
+            <p className="numberPink">  Choose the number of questions </p> 
+            <p className="numberPurple"> Choose your difficulty </p>
+            <p className="numberYellow"> Start test and the timer will start </p>
+            <p className="numberPink2"> Read the question and the alternetives, choose the one you think is right </p>
+            <p className="numberPurple2"> If your answer is right you'll get a point </p>
+            <p className="numberYellow2"> When quiz is done, your poins are counted and your score are put in the toplist </p>
             <h3> Good luck! </h3>
         </div>
     )
