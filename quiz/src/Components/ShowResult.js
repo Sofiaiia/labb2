8f2e13a92e19916891bcc1cb69b897217b4eb408
sofia södergårdh <sofia.s@aland.net>
2021-09-21 16:22:25 +0300

    lite mer animationer för resultatsidan

--- quiz/src/Components/ShowResult.js
+++ quiz/src/Components/ShowResult.js
@@ -14,24 +14,24 @@ import useSound from 'use-sound';
     const numOfQuestions = useSelector(selectQuestions);
     const time = useSelector(selectTime);
 
-    
+
 
     useEffect(()=>{
 
     },[]);
     
     return(
-        <div>
+        <div className="wrap">
             <img src={price} alt="price" height="300"/>
             <h2 className="resultHeader"> QUIZ COMPLETED </h2>
             <div className="boxResult">
-                <p> Total number of questions: {numOfQuestions} </p>
-                <p> Totalt points: {totalPoints} </p>
-                <p> Your time: {time}</p>
-                <p> That means you have {(totalPoints/numOfQuestions) * 100}% right </p>
+                <p className="text"> Total number of questions: {numOfQuestions} </p>
+                <p className="text"> Totalt points: {totalPoints} </p>
+                <p className="text"> Your time: {time}</p>
+                <p className="text"> That means you have {(totalPoints/numOfQuestions) * 100}% right </p>
             </div>
             <p className="lastHeading"> GOOD JOB {name} - </p>
-            <p> you can now find your result in the toplist! </p>
+            <p className="text"> you can now find your result in the toplist! </p>
         </div>
     )
  } 
