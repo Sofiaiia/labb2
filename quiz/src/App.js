d92a365c22c08af98af7e54d9d51828931f6c8bd
sofia södergårdh <sofia.s@aland.net>
2021-09-21 07:50:46 +0300

    fixat om navigeringen

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,19 +1,14 @@
-import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
+//import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
 import './styling/App.css';
-import Game from './views/Game';
-import Rules from './views/Rules';
-import Toplist from './views/Toplist';
+//import Game from './views/Game';
+//import Rules from './views/Rules';
+//import Toplist from './views/Toplist';
 import Start from './views/Start';
 
 function App() {
 
-  //HUR dölja menyn när spelet startar?!?!?
-  //Skulle vilja ha olika styling på startsidan och resten, går det??
-
-  return (
-    <div className="App">
-      <h1> QUIZ </h1>
-      <BrowserRouter>
+  /* 
+  <BrowserRouter>
         <div>
           <Link to="/"> Home</Link>
           <Link to="/game"> Play </Link>{' '}
@@ -27,6 +22,12 @@ function App() {
           </Switch>
         </div>
       </BrowserRouter>
+  */ 
+ 
+  return (
+    <div className="App">
+      <h1> QUIZ </h1>
+      <Start></Start>
     </div>
   );
 }
--- quiz/src/views/Start.js
+++ quiz/src/views/Start.js
@@ -1,10 +1,38 @@
-import React, {  } from 'react';
+import React, { useEffect, useState } from 'react';
+import Game from './Game';
+import Rules from './Rules';
+import Toplist from './Toplist';
 
 function Start(){
+
+    const [game,setGame] = useState(false);
+    const [rules,setRules] = useState(false);
+    const [toplist,setToplist] = useState(false);    
     
+    useEffect(()=>{
+        setGame(false);
+        setRules(false);
+        setToplist(false);
+    },[])
+
     return(
         <div>
-             START
+            {game
+            ?(<Game> </Game>)
+            :(rules
+                ?(<Rules> </Rules>)
+                :(toplist
+                    ?(<Toplist> </Toplist>)
+                    :(
+                        <div>
+                        <button onClick={() => setGame(true)}> PLAY GAME </button>
+                        <br/>
+                        <button onClick={() => setRules(true)}> RULES </button>
+                        <br/>
+                        <button onClick={() => setToplist(true)}> TOPLIST </button>
+                    </div>
+                    )))
+            }
         </div>
     );
 }
