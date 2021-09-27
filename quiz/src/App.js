276b783e4c451d9b6585073e51f0dfb51067be3e
sofia södergårdh <sofia.s@aland.net>
2021-09-27 08:13:03 +0300

    satte till en bild som logo

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,32 +1,12 @@
-//import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
 import './styling/App.css';
-//import Game from './views/Game';
-//import Rules from './views/Rules';
-//import Toplist from './views/Toplist';
 import Start from './views/Start';
+import quizLogo from './pictures/quizLogo.png';
 
 function App() {
-
-  /* 
-  <BrowserRouter>
-        <div>
-          <Link to="/"> Home</Link>
-          <Link to="/game"> Play </Link>{' '}
-          <Link to="/rules"> Rules </Link>{' '}
-          <Link to="/toplist"> Toplist </Link>{' '}
-          <Switch>
-            <Route exact path="/" component={Start}/>
-            <Route path="/game" component={Game} /> 
-            <Route path="/rules" component={Rules} /> 
-            <Route path="/toplist" component={Toplist} /> 
-          </Switch>
-        </div>
-      </BrowserRouter>
-  */ 
  
   return (
     <div className="App">
-      <h1> QUIZ </h1>
+      <img src={quizLogo} alt="logo" height="200"/>
       <Start></Start>
     </div>
   );
