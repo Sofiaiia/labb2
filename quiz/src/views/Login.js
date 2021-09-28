edbe79e077718c6fda670148e98c0c5b14aa765c
sofia södergårdh <sofia.s@aland.net>
2021-09-28 17:19:45 +0300

    fortästter med styling

--- quiz/src/views/Login.js
+++ quiz/src/views/Login.js
@@ -3,6 +3,10 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import '../styling/login.css';
+import EmailIcon from '@material-ui/icons/Email';
+import LockIcon from '@material-ui/icons/Lock';
+import { InputAdornment } from '@material-ui/core';
+//import GoogleIcon from '@material-ui/icons/Google';
 
 function Login(){
 
@@ -17,19 +21,25 @@ function Login(){
 
     return(
         <div>
-            <div className="sign-up-container">
+            <div className="sign-in-container">
                 <form onSubmit={() => signInEmail(email,password)}>
-                    <label> Email: </label>
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <h2 className="sign-in-header"> SIGN IN: </h2>
+                    <div className="input-box">
+                        <EmailIcon style={{ position:'absolute',paddingLeft:'5px'}}/>
+                        <input placeholder={"E-mail"} className="input-field" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
                     <br/>
-                    <label> Password: </label>
-                    <input  type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div className="input-box">
+                        <LockIcon style={{ position:'absolute',paddingLeft:'5px'}}/>
+                        <input placeholder="Password" className="input-field" type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
                     <br/>
-                    <button type="submit"> Login </button> 
+                    <button type="submit" className="login-button"> Login </button> 
+                    <br/>
+                    <button onClick={signInGoogle} className="google-button">Login with Google </button>
+                    <br/>
+                    <button> New user? Register here! </button>
                 </form>
-                <button onClick={signInGoogle}>Login with Google </button>
-                <br/>
-                <button> New user? Register here! </button>
             </div>
         </div>
     );
