fb5afd08f11a3e7c3f42175339d863d6666a0c2e
sofia södergårdh <sofia.s@aland.net>
2021-09-28 07:37:35 +0300

    installerat det jag behöver till firebase

--- quiz/src/Components/ShowResult.js
+++ quiz/src/Components/ShowResult.js
@@ -14,6 +14,7 @@ import useSound from 'use-sound';
 
     useEffect(()=>{
         props.handleStop();
+        //pusha resultat till firebase 
     },[]);
     
     return(
--- quiz/src/Components/questionsComp.js
+++ quiz/src/Components/questionsComp.js
@@ -62,29 +62,27 @@ function QuestionsComp(props){
                         <Alert icon={<ClearIcon fontSize="inherit"/>} severity="error" variant="filled" onClose={()=>setShow2(false)}>
                             <strong> WRONG ANSWER  </strong>
                         </Alert>
-                     </Collapse>
-                     
+                     </Collapse> 
                     <p className="questionBox">{fraga.question}</p> 
+                    <div className="">
                     {fraga.incorrect_answers.map((answer,index) => {
                             return(
                                 <div>
                                 { index === random
                                     ?(  <div>
                                             <button key={fraga.correct_answer} onClick={() =>checkAnswer(fraga.correct_answer,fraga.correct_answer)} className="answerButton">{fraga.correct_answer}</button>
-                                            <button key={answer} onClick={() => checkAnswer(answer,fraga.correct_answer)} className="answerButton">{answer}</button>
-                                            <br/>
+                                            <button key={answer} onClick={() => checkAnswer(answer,fraga.correct_answer)} className="answerButton">{answer}</button>                                      
                                         </div>
                                     )
                                     :(
                                         <div key={answer}>
                                             <button key={answer} onClick={() => checkAnswer(answer,fraga.correct_answer)} className="answerButton">{answer}</button>
-                                            <br/>
                                         </div>
                                     )}
                                 </div>
                             )
                      })}
-                    
+                    </div>
                   </div>  : "" 
                   ) 
         })}
